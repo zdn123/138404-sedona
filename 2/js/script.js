f3d3832a83634e13447d7c510ac6632792b5999d
Keks <keks@htmlacademy.ru>
2016-09-03 00:45:26 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,4 +1,13 @@
-ymaps.ready(function() {
+var link = document.querySelector(".promo__search");
+var popup = document.querySelector(".search-hotels");
+var open = documetn.querySelector(".search-hotels__hidden");
+popup.classlist.add("search-hotels__hidden");
+open.addeventlistener("click", function (event) {
+  event.preventDefault();
+  popup.classlist.remove("search-hotels__hidden");
+});
+
+ymaps.ready(function () {
   var myMap = new ymaps.Map('map-canvas', {
       center: [59.938631, -111.763806],
       zoom: 9
