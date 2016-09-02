53defe18a3c226085b0b317db15050465cd64094
Keks <keks@htmlacademy.ru>
2016-09-03 01:04:29 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,12 +1,11 @@
 var link = document.querySelector(".promo__search");
 var popup = document.querySelector(".search-hotels");
-var open = documetn.querySelector(".search-hotels__hidden");
-popup.classlist.add("search-hotels__hidden");
+var open = document.querySelector(".search-hotels__hidden");
+document.getElementById('search').classList.add('search-hotels__hidden');
 open.addeventlistener("click", function (event) {
   event.preventDefault();
   popup.classlist.remove("search-hotels__hidden");
 });
-
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map-canvas', {
       center: [59.938631, -111.763806],
