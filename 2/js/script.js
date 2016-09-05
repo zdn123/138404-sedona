91564d93bd5c5cf2098f52a08b76c543c8d3c37a
Keks <keks@htmlacademy.ru>
2016-09-05 20:04:35 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,24 +1,26 @@
-var link = document.querySelector(".promo__search");
-var popup = document.querySelector(".search-hotels");
-var open = document.querySelector(".search-hotels__hidden");
+var buttonChoose = document.querySelector('.promo__search');
+var form = document.querySelector('.search-hotels');
 document.getElementById('search').classList.add('search-hotels__hidden');
-open.addeventlistener("click", function (event) {
-  event.preventDefault();
-  popup.classlist.remove("search-hotels__hidden");
-});
+buttonChoose.onclick = function() {
+  if (form.classList.contains('search-hotels__hidden')) {
+    form.classList.remove('search-hotels__hidden');
+  } else {
+    form.classList.add('search-hotels__hidden');
+  }
+};
+
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map-canvas', {
-      center: [59.938631, -111.763806],
-      zoom: 9
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-      hintContent: 'Собственный значок метки',
-      balloonContent: 'Это красивая метка'
-    }, {
-      iconLayout: 'default#image',
-    });
-
+    center: [59.938631, -111.763806],
+    zoom: 12
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    hintContent: 'Собственный значок метки',
+    balloonContent: 'Это красивая метка'
+  }, {
+    iconLayout: 'default#image',
+  });
   myMap.geoObjects.add(myPlacemark);
 });
