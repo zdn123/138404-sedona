f8fdab52d2f67d42564442c7d2519f81e990d324
Keks <keks@htmlacademy.ru>
2016-10-03 19:25:18 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,6 +1,8 @@
 var buttonChoose = document.querySelector('.promo__search');
 var form = document.querySelector('.search-hotels');
-document.getElementById('search').classList.add('search-hotels__hidden');
+if (document.getElementById('search')){
+form.classList.add('search-hotels__hidden');
+};
 buttonChoose.onclick = function() {
   if (form.classList.contains('search-hotels__hidden')) {
     form.classList.remove('search-hotels__hidden');
@@ -8,19 +10,18 @@ buttonChoose.onclick = function() {
     form.classList.add('search-hotels__hidden');
   }
 };
-
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map-canvas', {
-    center: [59.938631, -111.763806],
-    zoom: 12
-  }, {
-    searchControlProvider: 'yandex#search'
-  }),
-  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    hintContent: 'Собственный значок метки',
-    balloonContent: 'Это красивая метка'
-  }, {
-    iconLayout: 'default#image',
-  });
+      center: [34.8697395, -111.7609896],
+      zoom: 12
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      balloonContent: 'Sedona'
+    }, {
+      iconLayout: 'default#image',
+    });
+
   myMap.geoObjects.add(myPlacemark);
 });
